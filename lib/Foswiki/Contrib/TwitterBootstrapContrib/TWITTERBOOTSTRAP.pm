f6ac6b97b2084b83e68261517c85c3475f06fb42
SvenDowideit <SvenDowideit@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-07-15 03:54:53 +0000

    Item11999: jqplugin enabled - JQREQUIRE{bootstrap}
    git-svn-id: http://svn.foswiki.org/trunk/TwitterBootstrapContrib@15150 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

---
+++ lib/Foswiki/Contrib/TwitterBootstrapContrib/TWITTERBOOTSTRAP.pm
@@ -0,0 +1,55 @@
+package Foswiki::Contrib::TwitterBootstrapContrib::TWITTERBOOTSTRAP;
+use strict;
+use warnings;
+
+use Foswiki::Plugins::JQueryPlugin ();
+our @ISA = qw( Foswiki::Plugins::JQueryPlugin::Plugin );
+
+use Foswiki::Plugins::JQueryPlugin::Plugin ();
+use Foswiki::Contrib::TwitterBootstrapContrib ();
+
+sub new {
+    my $class = shift;
+    my $session = shift || $Foswiki::Plugins::SESSION;
+   
+    my $this = $class->SUPER::new( 
+        $session,
+        name          => 'bootstrap',
+        version       => $Foswiki::Contrib::TwitterBootstrapContrib::RELEASE,
+        author        => 'twitter',
+        homepage      => 'http://twitter.github.com/bootstrap',
+        puburl        => '%PUBURLPATH%/%SYSTEMWEB%/TwitterBootstrapContrib/bootstrap',
+        documentation => "$Foswiki::cfg{SystemWebName}.TwitterBootstrapContrib",
+        summary       => $Foswiki::Contrib::TwitterBootstrapContrib::SHORTDESCRIPTION,
+        javascript    => [ "js/bootstrap.min.js" ],
+        css    => [ "css/bootstrap.min.css", "css/bootstrap-responsive.min.css" ]
+        );
+    
+    return $this;
+}
+
+
+sub renderCSS {
+    my ( $this, $text ) = @_;
+
+    $text =~ s/\.min// if $this->{debug};
+    $text .= '?version=' . $this->{version};
+    $text =
+"<link rel='stylesheet' href='$this->{puburl}/$text' type='text/css' media='all' />\n";
+
+    return $text;
+}
+
+sub renderJS {
+    my ( $this, $text ) = @_;
+
+    $text =~ s/\.min//
+      if ( $this->{debug} );
+      
+    $text .= '?version=' . $this->{version} if ( $this->{version} =~ '$Rev$' );
+    $text =
+      "<script type='text/javascript' src='$this->{puburl}/$text'></script>\n";
+    return $text;
+}
+
+1;
