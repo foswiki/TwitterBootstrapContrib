e5ad624c18193be336013757c63a99cb754ca46c
SvenDowideit <SvenDowideit@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-07-15 03:13:52 +0000

    Item11999: twitter bootstrap contrib
    git-svn-id: http://svn.foswiki.org/trunk/TwitterBootstrapContrib@15147 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

---
+++ test/unit/TwitterBootstrapContrib/TwitterBootstrapContribSuite.pm
@@ -0,0 +1,12 @@
+package 'TwitterBootstrapContrib'Suite;
+
+use Unit::TestSuite;
+our @ISA = qw( Unit::TestSuite );
+
+sub name {
+    '' TwitterBootstrapContrib'Suite ' }
+
+sub include_tests { qw(' TwitterBootstrapContrib'Tests );
+}
+
+1;
