2b608259c52e1a0c76059e1c0dfb57e4c0793bda
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-09-26 01:37:12 +0000

    Item11808: perltidy, v20120714 which has been installed on foswiki.org enforces a few new rules. Developers, PLEASE UPGRADE! This commit should help
    I've perltidy'ed everything which had a TIDY file.
    Also fixed all errors shown by perltidy (maybe still one encoding issue for Confluence)
    I've fixed everything I saw with obvious perl mistakes though. Doubt anybody
    will care, as most were due to a bogus Build::Contrib::createnewextension which
    Sven fixed recently.
    
    git-svn-id: http://svn.foswiki.org/trunk/TwitterBootstrapContrib@15429 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- test/unit/TwitterBootstrapContrib/TwitterBootstrapContribSuite.pm
+++ test/unit/TwitterBootstrapContrib/TwitterBootstrapContribSuite.pm
@@ -1,12 +1,14 @@
-package 'TwitterBootstrapContrib'Suite;
+package TwitterBootstrapContribSuite;
 
 use Unit::TestSuite;
 our @ISA = qw( Unit::TestSuite );
 
 sub name {
-    '' TwitterBootstrapContrib'Suite ' }
+    'TwitterBootstrapContribSuite';
+}
 
-sub include_tests { qw(' TwitterBootstrapContrib'Tests );
+sub include_tests {
+    qw(TwitterBootstrapContribTests);
 }
 
 1;
